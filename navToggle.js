a097f044e3714fa648369bd743d3cacd4824fe7d
eg744 <egillit@gmail.com>
2022-06-20 17:39:52 -0600

    added directories, simplify some modules

---
+++ navToggle.js
@@ -0,0 +1,26 @@
+function mobileNavToggle() {
+   document.addEventListener('DOMContentLoaded', () => {
+      const primaryNavBar = document.querySelector('.primary-navigation');
+
+      const navBarToggle = document.querySelector('.mobile-nav-toggle');
+
+      // navBarToggle.addEventListener('click', toggleNavBar);
+
+      navBarToggle.addEventListener('click', () => {
+         console.log('hello');
+         // Icon animation
+         navBarToggle.classList.toggle('change');
+
+         const isVisible = primaryNavBar.getAttribute('data-visible');
+         if (isVisible === 'false') {
+            primaryNavBar.setAttribute('data-visible', true);
+            navBarToggle.setAttribute('aria-expanded', true);
+         } else {
+            primaryNavBar.setAttribute('data-visible', false);
+            navBarToggle.setAttribute('aria-expanded', false);
+         }
+      });
+   });
+}
+
+module.exports = mobileNavToggle;
