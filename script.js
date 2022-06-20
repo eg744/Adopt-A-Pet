eb9b10943dc55869c014abf1665b810e6b584606
eg744 <egillit@gmail.com>
2022-06-20 15:04:16 -0600

    added jsdom for window and document events

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+GLOBAL.document = new JSDOM(html).window.document;
+
 // Express routing https://expressjs.com/en/api.html
 const express = require('express');
 const app = express();
@@ -19,4 +23,25 @@ app.post('/api', (req, res) => {
       message: 'success',
    });
 });
-window.addEventListener('load', () => {});
+
+// document.addEventListener('DOMContentLoaded', () => {
+//    const primaryNavBar = document.querySelector('.primary-navigation');
+
+//    const navBarToggle = document.querySelector('.mobile-nav-toggle');
+
+//    // navBarToggle.addEventListener('click', toggleNavBar);
+
+//    navBarToggle.addEventListener('click', () => {
+//       // Icon animation
+//       navBarToggle.classList.toggle('change');
+
+//       const isVisible = primaryNavBar.getAttribute('data-visible');
+//       if (isVisible === 'false') {
+//          primaryNavBar.setAttribute('data-visible', true);
+//          navBarToggle.setAttribute('aria-expanded', true);
+//       } else {
+//          primaryNavBar.setAttribute('data-visible', false);
+//          navBarToggle.setAttribute('aria-expanded', false);
+//       }
+//    });
+// });
